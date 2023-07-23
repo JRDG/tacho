f34f404385a011f3dd75c53bbf02681d5a2fe269
unknown <joaquinr@parabolt.net>
2023-07-23 02:06:46 -0300

    test deploy

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const blogRouter = require("./routes/BlogRoutes");
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/blogs", blogRouter);
+
+//configure mongoose
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/CRUD",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Connected to MongoDB");
+    }
+  }
+);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
